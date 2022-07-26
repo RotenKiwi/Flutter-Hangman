25558e94d020030a646b1dabfc33ff7cf4df4174
RotenKiwi <joshiparth212@gmail.com>
2022-07-26 22:49:18 +0530

    Keyboard Display

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,45 @@ class _HomeAppState extends State<HomeApp> {
                 !Game.selectedChar.contains(e.toUpperCase())))
                 .toList(),
           ),
+          SizedBox(
+            width: double.infinity,
+            height: 250.0,
+            child: GridView.count(
+              crossAxisCount: 7,
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              padding: EdgeInsets.all(8.0),
+              children: alphabets.map((e) {
+                return RawMaterialButton(
+                  onPressed: Game.selectedChar.contains(e)
+                      ? null // we first check that we didn't selected the button before
+                      : () {
+                    setState(() {
+                      Game.selectedChar.add(e);
+                      print(Game.selectedChar);
+                      if (!word.split('').contains(e.toUpperCase())) {
+                        Game.tries++;
+                      }
+                    });
+                  },
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: Text(
+                    e,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  fillColor: Game.selectedChar.contains(e)
+                      ? Colors.black87
+                      : Colors.blue,
+                );
+              }).toList(),
+            ),
+          )
         ],
       ),
     );
